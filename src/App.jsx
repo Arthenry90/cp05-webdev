7c351f6b34c2ee2dbeba7b20d19861c02cbb012b
Artur <arturhs2018@gmail.com>
2026-05-05 13:24:57 -0300

    feat: add apphilights

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import { useState } from 'react'
 import Header from './components/Header/Header';
 import HeroSection from './components/HeroSection/HeroSection';
+import AppHighlights from './components/AppHighlights/AppHighlights';
 
 function App() {
   return (
     <div className="min-h-screen bg-gray-50">
       <Header />
       <HeroSection />
+      
+      <AppHighlights />
     </div>
   )
 }
