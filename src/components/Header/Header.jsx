f53f71af88c2798bf68a1d6ae2bb4303cbed2b5c
Artur <arturhs2018@gmail.com>
2026-05-05 22:04:37 -0300

    fix: remove unnused logos

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,13 @@
 import React from 'react'
 import logo from "../../assets/logo.png"
-import logo2 from "../../assets/Gemini_Generated_Image_w7l3w0w7l3w0w7l3.png"
-import logo3 from "../../assets/Design sem nome.png"
+
 
 const Header = () => {
     return (
         <header className='w-full bg-white border-b border-gray-200 sticky top-0 z-50'>
             <div className="container mx-auto px-4 h-17 flex items-center justify-between">
                 <div className="text-xl font-bold text-blue-600">
-                    <img src={logo3} alt="Logo" className="h-17 " />
+                    <img src={logo} alt="Logo" className="h-17 " />
                 </div>
                 <nav className="hidden md:flex space-x-8 text-gray-600 font-medium">
                     <a href="#inicio" className="hover:text-blue-600 transition">Início</a>
